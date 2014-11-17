f4a07f7702b9dd2045344a23bdc08ebaaf24dda0
Uzi Eilon <PerfectoMobile256@gmail.com>
2014-11-17 17:47:11 -0500

    Update PerfectoTest.java

--- src/test/java/PerfectoTest.java
+++ src/test/java/PerfectoTest.java
@@ -28,7 +28,9 @@ public class PerfectoTest {
 		String host = Constants.PM_CLOUD;
 		String user = Constants.PM_USER;
 		String password = Constants.PM_PASSWORD;
-		MobileDriver driver = new MobileDriver(host, user, password);		
+		
+		MobileDriver PMdriver = new MobileDriver(host, user, password);		
+		
 		IMobileDevice device1 = PMdriver.getDevice("0149BCA71700D01F");
 		IMobileDevice device = PMdriver.getDevice("39F3DA5531ADBE2A05CFF4D65E43A2C38D3D595A");
 
