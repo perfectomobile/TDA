37c48d2e5f1eece269cf9ceb6fc5ed69e204435c
Uzi Eilon <PerfectoMobile256@gmail.com>
2014-12-09 13:18:23 -0500

    Update Constants.java

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -11,7 +11,7 @@ public class Constants
 
 	public static final String PM_CLOUD = "tda.perfectomobile.com";
 	
-	public static final String REPORT_LIB = "c:\\test\\";
+	public static final String REPORT_LIB = "c:\\TDA\\";
 
 }
 
